9d2d7bb8aff6a82722ac77063eea4dffb7697afb
jaanupriya <bhanupriya.ambaji@gmail.com>
2025-03-22 13:22:47 -0500

    my first commit

---
+++ infra.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = vars.aws_access_key
+  secret_key = vars.aws_secret_key
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "myfirstVPC"
+  }
+}
+
+resource "aws_subnet" "my_public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  availability_zone = "us-east-1a"
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_IGW"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.my_public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
